aa436b23d928c42d98048d256cfa4849339d9acb
tali42 <talisson_47@hotmail.com>
2024-02-25 19:24:02 -0300

    hosting and port

--- ProjetoBase/app.py
+++ ProjetoBase/app.py
@@ -135,4 +135,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    # app.run(debug = True)
+    app.run(debug = False, host="0.0.0.0", port="5000")
